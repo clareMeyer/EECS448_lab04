94374c64f7e8c27915d89738a3e380fdd447d080
cmeyer <claremeyer26@gmail.com>
2018-09-24 13:57:03 -0500

    first commit

---
+++ exercise1/phpFile.php
@@ -0,0 +1,11 @@
+<?php
+    for($i=1; $i<11; $i++){
+      echo "<tr>";
+      for($j=1; $j<11; $j++){
+        echo "<td>";
+        echo $j*$i;
+        echo "</td>";
+      }
+    echo "</tr>";
+    }
+  ?>
