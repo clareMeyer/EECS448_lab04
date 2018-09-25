4b273dedfdec97bd620f8503b95696a9b6319ada
cmeyer <claremeyer26@gmail.com>
2018-09-25 10:50:34 -0500

    added comments to exercise1

--- exercise1/phpFile.php
+++ exercise1/phpFile.php
@@ -1,4 +1,10 @@
 <?php
+/*
+phpFile.php
+Author: Clare Meyer
+Lab: 04; Exercise: 01
+function that populates table from html with the values for a tens times table
+ */
     for($i=1; $i<11; $i++){
       echo "<tr>";
       for($j=1; $j<11; $j++){
