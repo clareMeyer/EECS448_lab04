4ea6ed06e62525cdd033736bbb83db994d62cbab
cmeyer <claremeyer26@gmail.com>
2018-09-30 12:55:43 -0500

    finishing up exercise3

--- exercise3/customerBackend.php
+++ exercise3/customerBackend.php
@@ -1,10 +1,69 @@
+<style><?php include './style.css'; ?></style>
 <?php
 /*
 customerBackend.php
 Author: Clare Meyer
 Lab: 04; Exercise: 03
-
+processes the purchase and prints reciept
  */
+ $user = $_POST["username"];
+ $pass = $_POST["password"];
 
+ $shipping = $_POST["shipping"];
+ $quantMost = $_POST["most"];
+ $quantMiddle = $_POST["middle"];
+ $quantLeast = $_POST["least"];
+
+ $payMost = $quantMost * 1000000.00;
+ $payMiddle = $quantMiddle * 46320.05;
+ $payLeast = $quantLeast * 0.05;
+
+ $totalQuant = $quantMost + $quantMiddle + $quantLeast;
+ $total = $shipping + $payMost + $payMiddle + $payLeast;
+
+ echo "Welcome" . "</br>";
+ echo "Username: " . $user . "</br>";
+ echo "Password: " . $pass . "</br>";
+ echo "</br>";
+ echo "Reciept for payment: " . "</br>";
+ echo "</br>";
+
+ echo "<table border='1' class=php>
+  <tr>
+    <th class=php></td>
+    <th class=php>Quantity</td>
+    <th class=php>Cost Per Item</td>
+    <th class=php>Sub Total</td>
+  </tr>
+  <tr>
+    <td class=php>Item One</td>
+    <td class=php>$quantMost</td>
+    <td class=php>$1,000,000.00</td>
+    <td class=php>$ $payMost.00 </td>
+  </tr>
+  <tr>
+    <td class=php>Item Two</td>
+    <td class=php>$quantMiddle</td>
+    <td class=php>$46,320.05</td>
+    <td class=php>$ $payMiddle</td>
+  </tr>
+  <tr>
+    <td class=php>Item Three</td>
+    <td class=php>$quantLeast</td>
+    <td class=php>$0.05</td>
+    <td class=php>$ $payLeast</td>
+  </tr>
+  <tr>
+    <td class=php>Shipping</td>
+    <td class=php> </td>
+    <td class=php>$$shipping</td>
+    <td class=php>$ $shipping.00</td>
+ </tr>
+ <tr>
+  <td class=php>Total</td>
+  <td class=php>$totalQuant</td>
+  <td class=php> </td>
+  <td class=php>$ $total</td>
+ </table>";
 
  ?>
--- exercise3/formChecker.js
+++ exercise3/formChecker.js
@@ -2,5 +2,38 @@
 formChecker.js
 Author: Clare Meyer
 Lab: 04; Exercise: 03
-
+validates all the userinput from customerFrontend.html
  */
+
+function validate(){
+  /*gets all the information the user inputs from the html*/
+  var check=document.getElementById("username");
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  var rareQuant = document.getElementById("most").value;
+  var middleQuant = document.getElementById("middle").value;
+  var lastQuant = document.getElementById("least").value;
+  var shipping1 = document.getElementsByName("shipping");
+
+  /*makind sure they picked a shipping option*/
+  var shipping="";
+  for(var i=0; i < shipping1.length; i++){
+    if(shipping1[i].checked){
+      shipping=shipping1[i].value;
+    }
+  }
+
+  /*checks to make sure there are no blank or negative fields*/
+  if(username== "" || password=="" || rareQuant==""|| rareQuant<0 || middleQuant=="" || middleQuant<0 || lastQuant==""|| lastQuant<0||shipping==""){
+    if(rareQuant==""||middleQuant==""||lastQuant==""){
+      alert("quantities must be numbers");
+    }
+    else{
+      alert("CANNOT HAVE ANY BLANK FIELDS");
+    }
+  }
+  else if(!check.checkValidity()){
+    alert("must be a valid email");
+  }
+
+}
